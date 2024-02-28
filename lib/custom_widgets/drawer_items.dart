7eba122f4eb897d2d6217a0e2177425d2e3050d8
Ganeshdarshan Venkatraman Bhat <88202395+bhatganeshdarshan@users.noreply.github.com>
2024-02-28 14:43:38 +0530

    added drawer in home page

---
+++ lib/custom_widgets/drawer_items.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DrawerItems extends StatefulWidget {
+  const DrawerItems({super.key});
+
+  @override
+  State<DrawerItems> createState() => _DrawerItemsState();
+}
+
+class _DrawerItemsState extends State<DrawerItems> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
