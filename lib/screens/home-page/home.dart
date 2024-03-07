da98150062d272d8b38210a11aac2d8a59c778fd
Ganeshdarshan Venkatraman Bhat <88202395+bhatganeshdarshan@users.noreply.github.com>
2024-03-07 15:06:07 +0530

    pickup and drop location card added

--- lib/screens/home-page/home.dart
+++ lib/screens/home-page/home.dart
@@ -1,15 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Widget homePage() {
   return Container(
     padding: EdgeInsets.symmetric(vertical: 26, horizontal: 20),
     // color: Colors.amber,
-    height: 200,
+    height: 220,
     width: double.infinity,
     child: Card(
       elevation: 4,
       shadowColor: Colors.black,
-      child: Text("hello"),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Icon(
+                  Icons.gps_fixed,
+                  color: Color(0xff9BCF53),
+                ),
+                Text(
+                  "Enter Pickup Location",
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.add,
+                      color: Color(0xff9BCF53),
+                      size: 30,
+                    )),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Icon(
+                  Icons.gps_not_fixed_outlined,
+                  color: Color(0xff9BCF53),
+                ),
+                Text(
+                  "Enter Drop Location",
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.add,
+                      color: Color(0xff9BCF53),
+                      size: 30,
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
     ),
   );
 }
