c8b64c8ccbce626860fba5ba487f4d2a574a3d1a
Ganeshdarshan Venkatraman Bhat <88202395+bhatganeshdarshan@users.noreply.github.com>
2024-09-08 06:20:11 +0530

    Update app_constants.dart

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,5 +1,5 @@
-const String googleApiKey = "AIzaSyCemOhqkOMkdSrz17sSmI0AvyxYQDM93EY";
-const String supabaseUrl = "https://uikockdmivtqfcrehmro.supabase.co";
+const String googleApiKey = "apikey";
+const String supabaseUrl = "url";
 const String supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpa29ja2RtaXZ0cWZjcmVobXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2OTc2MTksImV4cCI6MjAyNTI3MzYxOX0.wmyM8mRKeydysDl_rAXdzgimNNBy1uR0zqBEz046PKA";
+    "sipabasekey";
 late String userId;
